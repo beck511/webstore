f92697c8e378d1cbe9c4f8a5fd8446a3562e811a
Derek.Beck <Derek.Beck@TRNGLA13045.fdmgroup.local>
2016-04-06 17:30:23 +0100

    06 1730

--- webstore/src/main/java/com/packt/webstore/service/ProductService.java
+++ webstore/src/main/java/com/packt/webstore/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.packt.webstore.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.packt.webstore.domain.Product;
 
@@ -10,4 +12,8 @@ import com.packt.webstore.domain.Product;
 
 public interface ProductService {
 	List <Product> getAllProducts();
+	List<Product> getProductsByCategory(String category);
+	Set<Product> getProductsByFilter(Map<String, List<String>> filterParams);
+	List <Product> getProductsByManufacturer(String manufacturer);
+	void addProduct(Product product);
 }
--- webstore/src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
+++ webstore/src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
@@ -1,7 +1,12 @@
 package com.packt.webstore.service.impl;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
 
 public class ProductServiceImpl {
@@ -20,4 +25,13 @@ public class ProductServiceImpl {
 	public void setProductRepository(ProductRepository productRepository) {
 		this.productRepository = productRepository;
 	}
+	public List<Product> getProductsByCategory(String category) {
+		  return productRepository.getProductsByCategory(category);
+		}
+	public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams) {
+	    return productRepository.getProductsByFilter(filterParams);
+	}
+	public void addProduct(Product product) {
+		   productRepository.addProduct(product);
+		}
 }
